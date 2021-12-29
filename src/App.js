210645c8688b0dab6bf653826f6075562d32ebc9
pacman766 <3334976@gmail.com>
2021-12-29 18:52:44 +0300

    add useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
+
+const countTotal = (num) => {
+  console.log('counting...');
+  return num + 10;
+};
 
 const Slider = (props) => {
   // useState hooks
   const [slide, setSlide] = useState(0);
   const [autoplay, setAutoplay] = useState(false);
 
+  // cashing function with useMemo hook
   const getSomeImages = useCallback(() => {
     console.log('fetching');
     return [
       'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
       'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
     ];
-  }, []);
+  }, [slide]);
 
   // hook useEffect = componentDidMount + componentDidUpdate +
   // componentWillUnmount in class components
@@ -30,20 +36,32 @@ const Slider = (props) => {
   function toggleAutoplay() {
     setAutoplay((autoplay) => !autoplay);
   }
+  // cashing object with useMemo hook
+  const total = useMemo(() => {
+    return countTotal(slide);
+  }, [slide]);
+
+  const style = useMemo(
+    () => ({
+      color: slide > 4 ? 'red' : 'black',
+    }),
+    [slide]
+  );
+
+  useEffect(() => {
+    console.log('style');
+  }, [style]);
 
   return (
     <Container>
       <div className="slider w-50 m-auto">
-        {/* {getSomeImages().map((url, i) => {
-          return (
-            <img key={i} className="d-block" w-100 src={url} alt="slide" />
-          );
-        })} */}
-
         <Slide getSomeImages={getSomeImages} />
 
         <div className="text-center mt-5">
           Active slide {slide} <br /> {autoplay ? 'auto' : null}
+        </div>
+        <div style={style} className="text-center mt-5">
+          Total slides: {total}
         </div>
         <div className="buttons mt-3">
           <button
