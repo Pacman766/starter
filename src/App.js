322931954b428b5bedbe0a9056546bee9e8e2315
unknown <3334976@gmail.com>
2021-12-29 11:17:03 +0300

    add useCallBack hook

--- src/App.js
+++ src/App.js
@@ -1,86 +1,27 @@
-import { Component, useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
-
-// class Slider extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       autoplay: false,
-//       slide: 0,
-//     };
-//   }
-
-//   changeSlide = (i) => {
-//     this.setState(({ slide }) => ({
-//       slide: slide + i,
-//     }));
-//   };
-
-//   toggleAutoplay = () => {
-//     this.setState(({ autoplay }) => ({
-//       autoplay: !autoplay,
-//     }));
-//   };
-
-//   render() {
-//     return (
-//       <Container>
-//         <div className="slider w-50 m-auto">
-//           <img
-//             className="d-block w-100"
-//             src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-//             alt="slide"
-//           />
-//           <div className="text-center mt-5">
-//             Active slide {this.state.slide} <br />{' '}
-//             {this.state.autoplay ? 'auto' : null}
-//           </div>
-//           <div className="buttons mt-3">
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={() => this.changeSlide(-1)}
-//             >
-//               -1
-//             </button>
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={() => this.changeSlide(1)}
-//             >
-//               +1
-//             </button>
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={this.toggleAutoplay}
-//             >
-//               toggle autoplay
-//             </button>
-//           </div>
-//         </div>
-//       </Container>
-//     );
-//   }
-// }
 
 const Slider = (props) => {
   // useState hooks
   const [slide, setSlide] = useState(0);
   const [autoplay, setAutoplay] = useState(false);
 
-  function logging() {
-    console.log('logged!');
-  }
+  const getSomeImages = useCallback(() => {
+    console.log('fetching');
+    return [
+      'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+      'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+    ];
+  }, []);
 
   // hook useEffect = componentDidMount + componentDidUpdate +
   // componentWillUnmount in class components
   useEffect(() => {
-    console.log('effect');
     document.title = `Slide: ${slide}`;
   }, [slide]);
 
-  useEffect(() => {
-    console.log('autoplay');
-  }, [autoplay]);
+  useEffect(() => {}, [autoplay]);
 
   function changeSlide(i) {
     setSlide((slide) => slide + i);
@@ -93,11 +34,14 @@ const Slider = (props) => {
   return (
     <Container>
       <div className="slider w-50 m-auto">
-        <img
-          className="d-block w-100"
-          src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-          alt="slide"
-        />
+        {/* {getSomeImages().map((url, i) => {
+          return (
+            <img key={i} className="d-block" w-100 src={url} alt="slide" />
+          );
+        })} */}
+
+        <Slide getSomeImages={getSomeImages} />
+
         <div className="text-center mt-5">
           Active slide {slide} <br /> {autoplay ? 'auto' : null}
         </div>
@@ -120,6 +64,21 @@ const Slider = (props) => {
         </div>
       </div>
     </Container>
+  );
+};
+
+const Slide = ({ getSomeImages }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(getSomeImages());
+  }, [getSomeImages]);
+  return (
+    <>
+      {images.map((url, i) => (
+        <img key={i} className="d-block" w-100 src={url} alt="slide" />
+      ))}
+    </>
   );
 };
 
